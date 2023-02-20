9528d6a3ff514d7d1bc604740397d02c223e9dff
Yorick-Ryu <2524964538@qq.com>
2023-02-20 20:51:00 +0800

    新增博客

---
+++ scripts/adsense.js
@@ -0,0 +1,3 @@
+hexo.extend.filter.register('theme_inject', function(injects) {
+    injects.head.raw('default', '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7736708165034885" crossorigin="anonymous"></script>');
+  });
